08da27d846aac78d3f9c5ac87b54bb99063abf58
Arseniy-Glebov <arseniy.glebov@gmail.com>
2025-05-09 20:32:52 +0300

    Upgraded animation with different frames per time period setting

--- Project/Anim_2.py
+++ Project/Anim_2.py
@@ -18,6 +18,8 @@ f.close()
 
 x = [sin(i) for i in fi]
 y = [-cos(i) for i in fi]
+t1 = [(t[i + 1] - t[i]) for i in range(len(t) - 1)]
+t1.append(t1[-1])
 
 plt.ion()
 fig = plt.figure(figsize=(5, 5))
@@ -27,12 +29,12 @@ ax.set_title("Анимация Маятника Дубошинского")
 ax.legend()
 plt.grid()
 
-for i in range(len(fi)):
+for i in range(0, len(fi), 1):  # <- количество пропусков кадров для ускорения
     line.set_data([x[i], 0], [y[i], 0])
     plt.draw()
     plt.gcf().canvas.flush_events()
 
-    time.sleep(0.001)
+    time.sleep(t1[i])
 
 plt.ioff()
 plt.show()
